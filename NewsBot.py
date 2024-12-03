eaa819a47b71358f451e4b473183520581a67417
Leandro <a@l5.lol>
2024-12-03 21:25:11 +0100

    20241203-2125
    fixed some issue

--- NewsBot.py
+++ NewsBot.py
@@ -1,160 +1,166 @@
 import discord
-from discord.ext.commands import Bot
 from discord.ext import commands
-
 import asyncio
-
 import datetime
-
 from threading import Thread, Lock
-
-from DataManager     import DataManager
+from DataManager import DataManager
 from SteamNewsFinder import SteamNewsFinder
-from SearchGames     import searchGames
+from SearchGames import searchGames
+from dotenv import load_dotenv
+import os
 
+# Load environment variables from .env file
+load_dotenv()
 
-# Constatnts
-token = "BOT_TOKEN"
-newsChannel="CHANNEL_ID"
-
-bot_prefix="!"
-fileName="gamenews.txt"	# File that will store all the news info.
-frequency=600			# The frequency with which the bot checks news
-
+# Constants loaded from the .env file
+token = os.getenv("DISCORD_TOKEN")
+newsChannel = os.getenv("NEWS_CHANNEL_ID")
+bot_prefix = "!"
+fileName = "gamenews.txt"  # File that will store all the news info.
+frequency = 600  # The frequency with which the bot checks news
 
 # Variables
 newsDataMutex = Lock()
-newsData 	= {}
+newsData = {}
 dataManager = None
-embedNews	= []	# Queue with news waiting to be posted in the news channel
-lastSearch  = []
+embedNews = []  # Queue with news waiting to be posted in the news channel
+lastSearch = []
 
-bot = commands.Bot(command_prefix=bot_prefix)
+# Define intents
+intents = discord.Intents.default()
+intents.message_content = True  # Enable the message content intent if needed
+
+# Create the bot with the specified intents
+bot = commands.Bot(command_prefix=bot_prefix, intents=intents)
 
 @bot.event
 async def on_ready():
-		global dataManager
-		global newsData
-		# Initialize variables
-		print("Bot Online!")
-		print("Initializating...")
-		dataManager = DataManager(fileName)
-		print("Loading news data... Storage: " + fileName)
-		newsData = dataManager.loadData()
-		print("Ready!")
-		bot.loop.create_task(backgroundNewsSeeking())
-		bot.loop.create_task(backgroundNewsSender())
+    global dataManager
+    global newsData
+    # Initialize variables
+    print("Bot Online!")
+    print("Initializing...")
+    dataManager = DataManager(fileName)
+    print("Loading news data... Storage: " + fileName)
+    newsData = dataManager.loadData()
+    print("Ready!")
+    bot.loop.create_task(backgroundNewsSeeking())
+    bot.loop.create_task(backgroundNewsSender())
 
 ################ REAL COMMANDS #######################
 
 @bot.command(pass_context=False)
-async def search(searchToken):
-	global lastSearch
+async def search(ctx, searchToken):
+    global lastSearch
 
-	result = await searchGames(searchToken,1)
+    result = await searchGames(searchToken, 1)
 
-	embed = discord.Embed(title="Resultados de la búsqueda '" + searchToken + "'")
+    embed = discord.Embed(title="Search results for '" + searchToken + "'")
 
-	i = 0
-	for r in result:
-		embed.add_field(name= str(i) + ". " + r['name'], value="[Página de steam](" + r['url'] + ")")
-		i = i+1
+    i = 0
+    for r in result:
+        embed.add_field(name=str(i) + ". " + r['name'], value="[Steam Page](" + r['url'] + ")")
+        i += 1
 
-	lastSearch = result
+    lastSearch = result
 
-	await bot.send_message(bot.get_channel(newsChannel), embed=embed)
-
-
-@bot.command(pass_context=False)
-async def state():
-	"""Print games that are currently in the list and their steam ids"""
-	global newsData
-	message = "Games on the list:\n"
-
-	for game in newsData:
-		message += " - " + game + " (" + str(newsData[game]['id']) + ")\n"
-	
-	await bot.send_message(bot.get_channel(newsChannel), message)
+    await ctx.send(embed=embed)
 
 @bot.command(pass_context=False)
-async def register(number):
-	global newsData
-	global lastSearch
+async def state(ctx):
+    """Print games that are currently in the list and their steam ids"""
+    global newsData
+    message = "Games on the list:\n"
 
-	if (lastSearch == []):
-		await bot.send_message(bot.get_channel(newsChannel), "Search something first! Don't use spaces on your search. There is an example:\n !search Hollow_Knight")
-		return
+    for game in newsData:
+        message += " - " + game + " (" + str(newsData[game]['id']) + ")\n"
 
-	if (int(number) < 0 ):
-		await bot.send_message(bot.get_channel(newsChannel), "Please enter a valid search number.")
-		return
+    await ctx.send(message)
 
-	try:
-		name     = lastSearch[int(number)]['name']
-		gameId   = lastSearch[int(number)]['id']
-	except IndexError:
-		await bot.send_message(bot.get_channel(newsChannel), "Please enter a valid search number.")
-		return
+@bot.command(pass_context=False)
+async def register(ctx, number: str):
+    global newsData
+    global lastSearch
 
-	newsData[name] = {}
-	newsData[name]['name'] = name
-	newsData[name]['id']   = gameId
-	newsData[name]['news'] = []
+    if not lastSearch:
+        await ctx.send("Search something first! Don't use spaces in your search. Example:\n !search Hollow_Knight")
+        return
 
-	newsDataMutex.acquire()
-	try:
-		dataManager.saveData(newsData)
-	finally:
-		newsDataMutex.release()
+    try:
+        index = int(number)
+        if index < 0 or index >= len(lastSearch):
+            raise ValueError("Index out of range.")
+        
+        name = lastSearch[index]['name']
+        gameId = lastSearch[index]['id']
+        
+    except ValueError:
+        await ctx.send("Please enter a valid search number (e.g., !register 0).")
+        return
+    except IndexError:
+        await ctx.send("Please enter a valid search number within range.")
+        return
 
-	message = "Game with id '" + gameId + "' and name '" + name + "' has been registered."
+    newsData[name] = {'name': name, 'id': gameId, 'news': []}
 
-	await bot.send_message(bot.get_channel(newsChannel), message)
-	Thread(target = getNews, args = (name,)).start()
+    with newsDataMutex:
+        dataManager.saveData(newsData)
 
-
+    message = f"Game with id '{gameId}' and name '{name}' has been registered."
+    
+    await ctx.send(message)
+    
+    Thread(target=getNews, args=(name,)).start()
 
 ###############   TASKS   ########################
 
 def getNews(gameName):
-	"""Check for new news, add them to embedNews, and update newsData. Multi-threading."""
-	global newsData
+    """Check for new news, add them to embedNews, and update newsData. Multi-threading."""
+    global newsData
 
-	game = newsData[gameName]
-	news = SteamNewsFinder.getGameNews(game['id'])
-	counter = 0
-	for newsItem in news:
-		newsItemDict = newsItem.__dict__
-		if (newsItemDict not in game['news']):
-			counter+=1
-			embedNews.append(newsItem.getEmbed(gameName))
-			game['news'].append(newsItemDict)
-	newsDataMutex.acquire()
-	try:
-		dataManager.saveData(newsData)
-	finally:
-		newsDataMutex.release()
-	print(gameName + " Done! " +  str(counter) + " news")
+    game = newsData[gameName]
+    news_items = SteamNewsFinder.getGameNews(game['id'])
+    
+    counter = 0
+    for newsItem in news_items:
+        newsItemDict = newsItem.__dict__
+        if newsItemDict not in game['news']:
+            counter += 1
+            embedNews.append(newsItem.getEmbed(gameName))
+            game['news'].append(newsItemDict)
+    
+    with newsDataMutex:
+        dataManager.saveData(newsData)
+    
+    print(f"{gameName} Done! {counter} new(s)")
 
 async def backgroundNewsSeeking():
-	"""This task is supposed to be run in the background. It will update the news data"""
-	global newsData
+    """This task is supposed to be run in the background. It will update the news data"""
+    global newsData
 
-	await asyncio.sleep(5) # 10 seconds delay
-	await bot.wait_until_ready()
-	while (1):
-		print("\nSeeking for news ... -----------(" + str(datetime.datetime.now().time()) + ")")
-		for game in newsData:
-			Thread(target = getNews, args = (game,)).start()
-		await asyncio.sleep(600)
+    await asyncio.sleep(5)  # Initial delay
+    await bot.wait_until_ready()
+    
+    while True:
+        print(f"\nSeeking for news ... -----------({datetime.datetime.now().time()})")
+        
+        for game in newsData:
+            Thread(target=getNews, args=(game,)).start()
+        
+        await asyncio.sleep(frequency)
 
 async def backgroundNewsSender():
-	"""This task is supposed to be run in the background. Check for news and posts it in the news channel"""
-	global embedNews
-	while(1):
-		if embedNews:
-			for embed in embedNews:
-				await bot.send_message(bot.get_channel(newsChannel), embed=embedNews.pop())
-		await asyncio.sleep(5)
+    """This task is supposed to be run in the background. Check for news and posts it in the news channel"""
+    global embedNews
+    
+    while True:
+        if embedNews:
+            while embedNews:
+                embed = embedNews.pop(0)
+                channel = bot.get_channel(int(newsChannel))
+                if channel:
+                    await channel.send(embed=embed)
+        
+        await asyncio.sleep(5)
 
 bot.run(token)
--- SteamNewsFinder.py
+++ SteamNewsFinder.py
@@ -1,31 +1,61 @@
 import json
-from urllib.request import urlopen
+import urllib.request
+from urllib.parse import quote
 from GameNews import GameNews
-
 import html2text
 
 idPlaceholder = "#GAMEID#"
 steamNewsUrl = "http://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=" + idPlaceholder + "&count=3&maxlength=300&format=json"
 
 class SteamNewsFinder(object):
-	"""Encapsulate the behaviour from game news seeking"""
+    """Encapsulate the behaviour from game news seeking"""
 
-	@staticmethod
-	def getGameNews(steamId):
-		"""Make the request for news to Steam api and transform the response into GameNews objects."""
-		news = []
+    @staticmethod
+    def getGameNews(steamId):
+        """Make the request for news to Steam API and transform the response into GameNews objects."""
+        news = []
 
-		url 	= steamNewsUrl.replace(idPlaceholder, str(steamId))
-		response = urlopen(url)
-		contentObject = json.loads(response.read()) 
-		for noticia in contentObject["appnews"]["newsitems"]:
-			urlSteam = noticia["url"]
-			urlReal  = urlopen(urlSteam).url
-			source   = noticia["feedlabel"]
-			title    = noticia["title"]
-			contents = html2text.html2text(noticia["contents"])
+        # Construct the URL for Steam API
+        url = steamNewsUrl.replace(idPlaceholder, str(steamId))
+        
+        # Define headers to mimic a browser request
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
 
-			newsItem = GameNews(urlReal, source, title, contents)
-			news.append(newsItem)
+        try:
+            # Make a request to the Steam API
+            req = urllib.request.Request(url, headers=headers)
+            with urllib.request.urlopen(req) as response:
+                contentObject = json.loads(response.read())
+            
+            # Process each news item in the response
+            for noticia in contentObject["appnews"]["newsitems"]:
+                urlSteam = noticia["url"]
+                encoded_urlSteam = quote(urlSteam, safe=':/')
+                
+                # Follow any redirects to get the real URL
+                req_real = urllib.request.Request(encoded_urlSteam, headers=headers)
+                urlReal = urllib.request.urlopen(req_real).url
+                
+                source = noticia["feedlabel"]
+                title = noticia["title"]
+                contents = html2text.html2text(noticia["contents"])
 
-		return news
+                newsItem = GameNews(urlReal, source, title, contents)
+                news.append(newsItem)
+
+        except urllib.error.HTTPError as e:
+            print(f"HTTP Error: {e.code} - {e.reason}")
+        except urllib.error.URLError as e:
+            print(f"URL Error: {e.reason}")
+        except json.JSONDecodeError as e:
+            print(f"JSON Decode Error: {e.msg}")
+
+        return news
+
+# Example usage:
+if __name__ == "__main__":
+    steam_id = 1142710  # Example Steam ID for Total War: WARHAMMER III
+    news_items = SteamNewsFinder.getGameNews(steam_id)
+    
+    for item in news_items:
+        print(item.format_news())
